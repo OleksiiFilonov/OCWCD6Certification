7efd9d85ebbb6d4ef14f1b6c3b968725dcc7d214
Oleksii Filonov <asfilonov@gmail.com>
2013-09-08 11:46:39 +0300

    Servlet to display different information: ServletContext.getContextPath - path to application, request.getServletPath = URI = current path to servlet in browser

---
+++ src/main/java/com/example/annotations/PathsInformationServlet.java
@@ -0,0 +1,33 @@
+package com.example.annotations;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(description = "This is just a servlet to display Context path and Context value", urlPatterns = {
+		"/informationServlet", "/informationServlet2", "*.info" }, displayName = "PathsInformationServlet servlet Display Name", name = "PathsInformation")
+public class PathsInformationServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException,
+			IOException {
+		final PrintWriter writer = response.getWriter();
+		writer.println("<html><body>");
+		writer.println(String.format("<h1>Contex path getServletContext().getContextPath(): \"%s\"</h1>",
+				getServletContext().getContextPath()));
+		writer.println(String.format("<h1>Servlet path request.getServletPath(): \"%s\"</h1>", request.getServletPath()));
+		writer.println(String.format("<h1>Server Info getServletContext().getServerInfo() : \"%s\"</h1>",
+				getServletContext().getServerInfo()));
+		writer.println(String.format("<h1>Servlet Info getServletInfo() : \"%s\"</h1>", getServletInfo()));
+		writer.println(String.format("<h1>Request URI request.getRequestURI() : \"%s\"</h1>", request.getRequestURI()));
+		writer.println(String.format("<h1>Request URL request.getRequestURL() : \"%s\"</h1>", request.getRequestURL()));
+		writer.println("</body></html>");
+	}
+}
